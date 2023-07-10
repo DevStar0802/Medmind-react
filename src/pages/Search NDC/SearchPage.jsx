d583fd8c1d013c6d9e8bf868b478ef5162abfa98
DevStar0802 <ssantiagogutierrezzz@gmail.com>
2023-07-10 16:56:24 +0500

    Search Page changes

--- src/pages/Search NDC/SearchPage.jsx
+++ src/pages/Search NDC/SearchPage.jsx
@@ -18,10 +18,33 @@ const SearchPage = () => {
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState("false");
   const [matchNdc, setMatchNdc] = useState("");
+  const [results, setResults] = useState([]);
   const navigate = useNavigate();
 
+  const getresults = async (searchData) => {
+    if (searchData.length > 2) {
+      try {
+        setMatchNdc(searchData);
+        setLoading("true");
+        await axios
+          .get(
+            `http://ec2-44-215-42-63.compute-1.amazonaws.com:8080/api/search?query=${searchData}`
+          )
+          .then((res) => {
+            console.log("REsults", res.data.body);
+            setResults(res.data.body);
+          });
+
+        setLoading("false");
+      } catch (error) {
+        setLoading("false");
+        console.log("error", error);
+      }
+    }
+  };
   const getApi = async (searchData) => {
     try {
+      console.log("searchData", searchData);
       setMatchNdc(searchData);
       setLoading("true");
       await axios
@@ -40,7 +63,7 @@ const SearchPage = () => {
     }
   };
 
-  const handelSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const form = event.target;
     let ndc = form.formBasicEmail.value.trim();
@@ -65,37 +88,73 @@ const SearchPage = () => {
     } else if (ndc.length > 1) await getApi(ndc);
   };
 
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    console.log("handleInputChange :", inputValue);
+    if (inputValue.length > 2) {
+      getresults(inputValue);
+    }
+  };
+  const handleQuery = (ndc) => {
+    getApi(ndc);
+  };
+
   return (
     <>
       <VStack mt="10px">
-        <Heading textAlign="center">Search By Using NDC Number</Heading>
-        <Form onSubmit={handelSubmit}>
+        <Heading textAlign="center">Search </Heading>
+        <Form onSubmit={handleSubmit} style={{ position: "relative" }}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Tooltip label="Enter 11 Digit NDC" placement="auto-start">
+            <Tooltip label="Enter your Query" placement="auto-start">
               <Form.Control
                 type="text"
-                placeholder="Enter 11 Digit NDC"
+                onChange={handleInputChange}
+                placeholder="Search..."
                 style={{ width: "300px" }}
               />
             </Tooltip>
+            {results && results.length > 0 && (
+              <>
+                <Box
+                  position="absolute"
+                  bg="white"
+                  zIndex="999"
+                  p="3"
+                  boxShadow="0 3px 10px rgb(0 0 0 / 0.2)"
+                  borderRadius="5px"
+                  maxH="600px"
+                  overflowY="scroll"
+                >
+                  {results.map((result, index) => (
+                    <Box
+                      borderBottom="1px solid #dadada"
+                      key={index}
+                      align="start"
+                      gap={3}
+                      px="5px"
+                      cursor="pointer"
+                      onClick={() => {
+                        getApi(result.ndc);
+                        setResults([]);
+                      }}
+                    >
+                      <Text style={{ fontWeight: "600" }} margin="1px">
+                        {result.name}
+                      </Text>
+                      <Text margin="1px">{result.ndc}</Text>
+                    </Box>
+                  ))}
+                </Box>
+              </>
+            )}
           </Form.Group>
-          <div className="d-flex justify-content-center align-items-center">
-            {loading === "true" ? (
+          {loading === "true" && (
+            <div className="d-flex justify-content-center align-items-center">
               <Spinner animation="border" role="status">
                 <span className="visually-hidden">Loading...</span>
               </Spinner>
-            ) : (
-              <Button
-                className="w-50 opacity-75 "
-                variant="success"
-                type="submit"
-                loading={loading}
-              // disabled={loading.toString()}
-              >
-                Search
-              </Button>
-            )}
-          </div>
+            </div>
+          )}
         </Form>
 
         {product && product.length > 0 ? (
@@ -248,9 +307,8 @@ const SearchPage = () => {
           </>
         ) : (
           <Text fontWeight="bold" fontSize="30px" color="red.500">
-            Enter NDC Number
+            Enter Your Query
           </Text>
-
         )}
         <VStack
           spacing={5}
@@ -261,59 +319,147 @@ const SearchPage = () => {
           p={5}
         >
           <Flex bg="gray.100" p={4} borderRadius="md" flexDirection="column">
-            <Heading as="h1" size="xl" textAlign="center" color="gray.600" fontSize="20px">
-
+            <Heading
+              as="h1"
+              size="xl"
+              textAlign="center"
+              color="gray.600"
+              fontSize="20px"
+            >
               Welcome to MedMind - Your Source for Affordable Medications
             </Heading>
             <Text fontSize="md" textAlign="center" color="gray.500">
-              At our website, we are dedicated to revolutionizing access to affordable medications. We understand the financial burden that high drug costs can place on individuals and families, and we believe that everyone should have access to the medications they need without breaking the bank. That's why we have partnered directly with drug manufacturers to provide you with the lowest cost medications available.
+              At our website, we are dedicated to revolutionizing access to
+              affordable medications. We understand the financial burden that
+              high drug costs can place on individuals and families, and we
+              believe that everyone should have access to the medications they
+              need without breaking the bank. That's why we have partnered
+              directly with drug manufacturers to provide you with the lowest
+              cost medications available.
             </Text>
-            <Heading as="h1" size="xl" textAlign="center" color="gray.600" fontSize="20px">
+            <Heading
+              as="h1"
+              size="xl"
+              textAlign="center"
+              color="gray.600"
+              fontSize="20px"
+            >
               Our Approach
             </Heading>
             <Text fontSize="md" textAlign="center" color="gray.500">
-              We have taken a unique approach to tackle the rising costs of medications. By collaborating directly with reputable drug manufacturers, we are able to cut out unnecessary intermediaries and pass on the savings directly to you. This allows us to offer a wide range of high-quality medications at significantly lower prices compared to traditional pharmacy channels.
+              We have taken a unique approach to tackle the rising costs of
+              medications. By collaborating directly with reputable drug
+              manufacturers, we are able to cut out unnecessary intermediaries
+              and pass on the savings directly to you. This allows us to offer a
+              wide range of high-quality medications at significantly lower
+              prices compared to traditional pharmacy channels.
             </Text>
-            <Heading as="h1" size="xl" textAlign="center" color="gray.600" fontSize="20px">
+            <Heading
+              as="h1"
+              size="xl"
+              textAlign="center"
+              color="gray.600"
+              fontSize="20px"
+            >
               Quality and Safety
             </Heading>
             <Text fontSize="md" textAlign="center" color="gray.500">
-              We prioritize your health and well-being above all else. Rest assured that the medications we offer are sourced directly from authorized manufacturers, ensuring their authenticity and quality. We work exclusively with trusted pharmaceutical companies that adhere to stringent quality control measures and regulatory standards.
+              We prioritize your health and well-being above all else. Rest
+              assured that the medications we offer are sourced directly from
+              authorized manufacturers, ensuring their authenticity and quality.
+              We work exclusively with trusted pharmaceutical companies that
+              adhere to stringent quality control measures and regulatory
+              standards.
             </Text>
-            <Heading as="h1" size="xl" textAlign="center" color="gray.600" fontSize="20px">
+            <Heading
+              as="h1"
+              size="xl"
+              textAlign="center"
+              color="gray.600"
+              fontSize="20px"
+            >
               Convenience and Ease
             </Heading>
             <Text fontSize="md" textAlign="center" color="gray.500">
-              We understand that obtaining affordable medications should be a hassle-free experience. Our user-friendly website is designed to make the ordering process quick, easy, and secure. Simply browse our extensive selection of medications, add them to your cart, and proceed to checkout. We offer multiple payment options and ensure discreet packaging for your privacy.
+              We understand that obtaining affordable medications should be a
+              hassle-free experience. Our user-friendly website is designed to
+              make the ordering process quick, easy, and secure. Simply browse
+              our extensive selection of medications, add them to your cart, and
+              proceed to checkout. We offer multiple payment options and ensure
+              discreet packaging for your privacy.
             </Text>
-            <Heading as="h1" size="xl" textAlign="center" color="gray.600" fontSize="20px">
+            <Heading
+              as="h1"
+              size="xl"
+              textAlign="center"
+              color="gray.600"
+              fontSize="20px"
+            >
               Expert Guidance and Support
             </Heading>
             <Text fontSize="md" textAlign="center" color="gray.500">
-              We believe that access to affordable medications should be accompanied by professional guidance and support. Our team of knowledgeable pharmacists is available to answer any questions you may have about your medications, potential side effects, or drug interactions. We are committed to ensuring that you have the information you need to make informed decisions about your healthcare.
+              We believe that access to affordable medications should be
+              accompanied by professional guidance and support. Our team of
+              knowledgeable pharmacists is available to answer any questions you
+              may have about your medications, potential side effects, or drug
+              interactions. We are committed to ensuring that you have the
+              information you need to make informed decisions about your
+              healthcare.
             </Text>
-            <Heading as="h1" size="xl" textAlign="center" color="gray.600" fontSize="20px">
+            <Heading
+              as="h1"
+              size="xl"
+              textAlign="center"
+              color="gray.600"
+              fontSize="20px"
+            >
               Privacy and Security
             </Heading>
             <Text fontSize="md" textAlign="center" color="gray.500">
-              Protecting your personal information is of utmost importance to us. We employ the latest security measures and encryption protocols to safeguard your data. You can trust that your privacy is our top priority, and we will never share your information with third parties without your explicit consent.
+              Protecting your personal information is of utmost importance to
+              us. We employ the latest security measures and encryption
+              protocols to safeguard your data. You can trust that your privacy
+              is our top priority, and we will never share your information with
+              third parties without your explicit consent.
             </Text>
-            <Heading as="h1" size="xl" textAlign="center" color="gray.600" fontSize="20px">
+            <Heading
+              as="h1"
+              size="xl"
+              textAlign="center"
+              color="gray.600"
+              fontSize="20px"
+            >
               Our Mission
             </Heading>
             <Text fontSize="md" textAlign="center" color="gray.500">
-              Our mission is to empower individuals to take control of their health by providing access to affordable medications without compromising on quality. We believe that no one should have to choose between their health and their financial well-being.
+              Our mission is to empower individuals to take control of their
+              health by providing access to affordable medications without
+              compromising on quality. We believe that no one should have to
+              choose between their health and their financial well-being.
             </Text>
-            <Heading as="h1" size="xl" textAlign="center" color="gray.600" fontSize="20px">
+            <Heading
+              as="h1"
+              size="xl"
+              textAlign="center"
+              color="gray.600"
+              fontSize="20px"
+            >
               Join Us Today
             </Heading>
             <Text fontSize="md" textAlign="center" color="gray.500">
-              We invite you to join us in our mission to make affordable medications accessible to all. Together, we can break down the barriers that prevent individuals from accessing the treatments they need. Explore our website, find the medications you require, and experience the convenience and savings firsthand.
+              We invite you to join us in our mission to make affordable
+              medications accessible to all. Together, we can break down the
+              barriers that prevent individuals from accessing the treatments
+              they need. Explore our website, find the medications you require,
+              and experience the convenience and savings firsthand.
             </Text>
             <Text fontSize="md" textAlign="center" color="gray.500">
-              At our website, we are committed to delivering affordable medications, exceptional customer service, and a brighter, healthier future for all. Start saving on your prescription medications today and take the first step towards improved well-being.
+              At our website, we are committed to delivering affordable
+              medications, exceptional customer service, and a brighter,
+              healthier future for all. Start saving on your prescription
+              medications today and take the first step towards improved
+              well-being.
             </Text>
-
           </Flex>
         </VStack>
       </VStack>
