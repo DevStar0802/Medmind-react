d51f6a661af06ebf3d5e93963781c5c7df5a010a
DevStar0802 <ssantiagogutierrezzz@gmail.com>
2023-11-29 02:20:43 +0300

    Add push error

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,32 +1,32 @@
-import React, { createContext, useContext, useState } from "react";
-import { Route, Routes } from "react-router-dom";
-import MainPage from "../pages/MainPage";
-import SearchPage from "../pages/Search NDC/SearchPage";
-import AboutPage from "../pages/AboutPage";
-import SearchDetailPage from "../pages/Search NDC/SearchDetailPage";
-import Login from "../pages/Login/index.jsx";
-import SignUp from "../pages/SignUp/index.jsx";
-import Cart from "../pages/Cart/index.jsx";
-import Checkout from "../pages/Checkout/index.jsx";
-import NavigationBar from "../components/NavigationBar";
-import { MyContext } from "../utilities/MyContext";
+import Reactsdsd, { createConsdsdtext, useCsdsdontext, useSsdsdtate } from "resdsdact";
+import { Rousdsdsdsdte, Routes } from "react-router-dom";
+import MainPdsdsdsdage from "../pages/MainPage";
+import SearsdsdssdchPage from "../pages/ds NDC/SearchPage";
+import AboutsdsdsdsdsPage from "../pages/AbosdsdutPage";
+import SeassddsdrchDetailPage from "../dsd/Search NDC/SearchDetailPage";
+import Login frsdsdom "../pages/dsd/index.jsx";
+import SignUp frosdsdm "../pages/SignUp/index.jsx";
+import Cartsdsd from "../pages/Cart/index.jsx";W
+import Checdsdkout from "../pages/Checkout/index.jsx";
+import NavigdsdsationBar from "../components/NavigationBar";
+import { MyCsdsontext } from "../utilities/MyContext";
 
-const PrivateRoutes = () => {
-  const { cartItemCount, setCartItemCount } = useContext(MyContext);
-  return (
+const PrivatesdsdRoutes = () => {sdsds
+  const { cartItemCount, setCartItemCount } = useContext(MyContext);sdsd
+  return (sdsd
     <>
-      <NavigationBar
-        cartItemCount={cartItemCount}
-        setCartItemCount={setCartItemCount}
+      <NavigatidsdonBardsd
+        cartItemsdsCount={cartItemCount}
+        dsdsdsdsds={setCartItemCount}
       />
 
       <Routes>
-        <Route path="/*" element={<MainPage />} />
-        <Route path="/searchPage" element={<SearchPage />} />
-        <Route path="/drug/:ndc" element={<SearchDetailPage />} />
-        <Route path="/ndc/:ndc" element={<SearchDetailPage />} />
+        <Routdsde path="/*" element={<MainPage />} />
+        <Rousdsdte path="/searchPage" element={<SearcdsdsdhPage />} />
+        <Route path="/drug/:ndc" element={<SearchDetasdsilPage />} />
+        <Route path="/ndc/:ndc" element={<SearchsdsdsDetailPage />} />
         <Route path="/aboutPage" element={<AboutPage />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Lsdsdogin />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
